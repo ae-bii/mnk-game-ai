f973699d7e0807c80bc74b455bdeb0515fb16e06
Anupam Bhakta <anupambhakta1@gmail.com>
2023-07-09 18:21:42 -0500

    abstracted classes to other files

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,14 +1,43 @@
-import random
-
 class MNKGame:
-    def __init__(self, m, n, k):
+    def __init__(self, m, n, k, ai_player):
         self.m = m
         self.n = n
         self.k = k
         self.board = [[' ' for _ in range(n)] for _ in range(m)]
         self.current_player = 'X'
+        self.ai_player = ai_player
+
+    def start(self):
+
+        while not self.is_game_over():
+            self.print_board()
+
+            # Human player's turn
+            while True:
+                row = int(input('Enter the row (0 to {}): '.format(self.m - 1)))
+                col = int(
+                    input('Enter the column (0 to {}): '.format(self.n - 1)))
+                if self.make_move(row, col):
+                    break
+                print('Invalid move. Try again.')
+                print()
+
+            if self.is_game_over():
+                break
+
+            # AI player's turn
+            row, col = self.ai_player.get_move(self)
+            self.make_move(row, col)
+
+        self.print_board()
+        winner = self.get_winner()
+        if winner:
+            print('Player', winner, 'wins!')
+        else:
+            print('It\'s a draw!')
 
     def print_board(self):
+        print()
         for row in self.board:
             print('|'.join(row))
         print()
@@ -63,48 +92,3 @@ class MNKGame:
                         return self.board[row][col]
 
         return None
-
-class AIPlayer:
-    def __init__(self, symbol):
-        self.symbol = symbol
-
-    def get_move(self, game):
-        m = game.m
-        n = game.n
-        for row in range(m):
-            for col in range(n):
-                if game.is_valid_move(row, col):
-                    return row, col
-
-# Example usage
-m = 3
-n = 3
-k = 3
-
-game = MNKGame(m, n, k)
-ai_player = AIPlayer('O')
-
-while not game.is_game_over():
-    game.print_board()
-
-    # Human player's turn
-    while True:
-        row = int(input('Enter the row (0 to {}): '.format(m - 1)))
-        col = int(input('Enter the column (0 to {}): '.format(n - 1)))
-        if game.make_move(row, col):
-            break
-        print('Invalid move. Try again.')
-
-    if game.is_game_over():
-        break
-
-    # AI player's turn
-    row, col = ai_player.get_move(game)
-    game.make_move(row, col)
-
-game.print_board()
-winner = game.get_winner()
-if winner:
-    print('Player', winner, 'wins!')
-else:
-    print('It\'s a draw!')
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import game_logic as gl
+import ai_player as ap
+
+
+# Example usage
+m = int(input('Enter a value for m greater than 0: '))
+n = int(input('Enter a value for n greater than 0: '))
+k = int(input('Enter a value for k greater than 0: '))
+
+ai_player = ap.AIPlayerOptimal('O')
+game = gl.MNKGame(m, n, k, ai_player)\
+
+game.start()
